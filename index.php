eb5a2de9b993e68cacdf3f0fda9605fdd4a5698b
tildawiklund <tildawiklund2002@gmail.com>
2026-09-10 13:51:51 +0200

    more updates to index

--- index.php
+++ index.php
@@ -19,20 +19,35 @@ include 'closeDB.php';
 <!DOCTYPE html> 
 <html>
 <body>
-    <form action="/action_page.php"> <!--what is this -->
+    <h1> Welcome! </h1>
+    <h3> On this webpage you can insert your own movie ratings. </h3>
+    <h3> The ratings are from 1-5, with 5 being the best. Have fun :)</h3>
+
+    <form action="/action_page.php"> <!--we have to change this, apperantly something in tutorials -->
         <label for="moviename">Movie Name:</label><br>
         <input type="text" id="moviename" name="moviename"><br>
 
         <label for="year">Year:</label><br>
         <input type="text" id="year" name="year"><br>
 
-        <label for="rating">Year:</label><br>
+        <label for="rating">Rating:</label><br>
         <select id="rating" name="rating">
             <option value = 1> 1 </option>
             <option> 2 </option>
             <option> 3 </option>
             <option> 4 </option>
             <option> 5 </option>
+        </select><br><br>
+
+
+        <!--do we have to connect to the database to choose the genres, or can we just do it here? -->
+        <!-- value = whats inserted to db (we need to take into account the FK) -->
+        <label for="genre">Genre:</label> <br>
+        <select id="genre" name="genre">
+            <option value = 1> Action/Adventure </option>
+            <option value = 2> Comedy </option>
+            <option value = 3> Drama </option>
+            <option value = 4> Fantasy/Sci-Fi </option>
         </select><br><br>
 
         <input type="submit" value="Submit">
