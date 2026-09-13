f572d61403a147ae97277c22c7ef83f0d21a2c4f
edithnilsson <edithenn123@gmail.com>
2026-09-13 10:07:05 +0200

    make it look nicer

--- showmovies.php
+++ showmovies.php
@@ -10,22 +10,57 @@ if(isset($_POST['insert_new']))
 
 ?>
 
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <!-- make it so that the page width matches the users screen size -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- loads a CSS library, bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Movie review</title>
+
+    <style>
+        body{
+            padding: 50px ; 
+        }
+        .container{
+            max-width: 1000px ;
+            padding: 50px ;
+            margin: 0 auto;
+        } 
+        .form-group{
+            margin-bottom: 30 px
+        }
+    </style>
+
+</head>
+
+<!-- code to create HTML table -->
 <body>
+<div class="container">
     <h1> Table of Movie Rating </h1>
     <br>
 
+<!-- creates a search button -->
+<form method="post">
+    <label for="site-search">Search the site:</label>
+    <input type="search" id="site-search" name="search_button" placeholder = Search />
+    <button>Search</button>
+</form>
+
 <!-- code to create HTML table -->
-<table>
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Year</th>
-            <th>Genre</th>
-            <th>Rating</th>
-        </tr>
-    </thead>
+<table class="table table-striped"> <!-- make the rows striped -->
+  <thead class="table-dark"> <!-- adds a dark table header-->
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Year</th>
+      <th scope="col">Genre</th>
+      <th scope="col">Rating</th>
+    </tr>
+  <thead> 
 
     <tbody>
         <?php
@@ -76,18 +111,12 @@ if(isset($_POST['insert_new']))
 </table>
 
 
-<form method="post">
-    <label for="site-search">Search the site:</label>
-    <input type="search" id="site-search" name="search_button" placeholder = Search />
-    <button>Search</button>
-</form>
-
-
-
 <!--make a button to switch between pages -->
     <form method="post">
         <input type="submit" name=insert_new value="Insert new values">
     </form>
+
+</div>
 
 </body>
 </html>
