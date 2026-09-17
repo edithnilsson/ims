c2e2df5a9deb1328ea78028a238d67febe15b55b
tildawiklund <tildawiklund2002@gmail.com>
2026-09-17 11:09:14 +0200

    added commenting

--- insert.php
+++ insert.php
@@ -4,32 +4,25 @@
 <?php
 include 'db.php';
 
-
-
-    # i guess we have to do something like this
-    # INSERT INTO movies (mname, myear, mgenreid, mrating)
-    # VALUES (value1, value2, value3, ...);
-
+    # if button to submit info:
     if(isset($_POST['submit']))
     {
-
-  
-    
-
     # fetch data from POST request
     $moviename = $_POST['moviename'];
     $year = $_POST['year'];
     $rating = $_POST['rating'];
     $genre = $_POST['genre'];
 
+    # use placeholders to protect against sql injection
     $sql = "INSERT INTO movies(mname, myear, mgenreid, mrating) VALUES (?, ?, ?, ?)";
 
     $stmt = $conn->prepare($sql);
 
-    // s for string
-    $stmt->bind_param("ssii", $moviename, $year, $genre, $rating);
+    # s for string, i for integer
+    $stmt->bind_param("siii", $moviename, $year, $genre, $rating);
     $result = $stmt->execute();
 
+    # echos how it went
     if ($result) {
         echo "New record created successfully";
     } else {
